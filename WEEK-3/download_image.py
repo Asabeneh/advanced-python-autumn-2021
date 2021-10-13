834f8ea2e3b66e28a46411a4f06f312a3de49ec5
Asabeneh <asabeneh@gmail.com>
2021-10-13 22:58:53 +0300

    script added

--- WEEK-3/download_image.py
+++ WEEK-3/download_image.py
@@ -1,13 +1,16 @@
 import requests
+import json
 from fetch_api import fetch_api
 url = 'https://api.thecatapi.com/v1/breeds'
 data = fetch_api(url)
+with open('cats.json', 'w', encoding='utf-8') as f:
+    json.dump(data, f, ensure_ascii=False, indent=4)
 
-for cat in data:
+""" for cat in data:
     if 'image' in cat:
         img_url = cat['image']['url']
         img = requests.get(img_url).content
         name = cat["name"]
         with open(f'images/{name}.jpg', 'wb') as f:
-            f.write(img)
+            f.write(img) """
 
